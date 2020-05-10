3773caa49805dee0e91e78ee87182ffc433f88b4
Sadman Ahmed Shanto <sadman-ahmed.shanto@ttu.edu>
2020-05-09 20:36:10 -0500

    update pic

--- downloads-organizer-master/downloadsorganizer/attributes.py
+++ downloads-organizer-master/downloadsorganizer/attributes.py
@@ -5,7 +5,7 @@ USERNAME = os.getlogin()
 DOWNLOADS_DIRECTORY = '/Users/' + USERNAME + '/Downloads/'
 
 EXTENSIONS_DICT = {
-    'image': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg'],
+    'image': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.heic'],
     'video': ['.mp4', '.webm', '.flv', '.avi', '.mov', '.qt', '.m4v'],
     'audio': ['.mp3', '.wav', '.m4a'],
     'torrent': ['.torrent'],
